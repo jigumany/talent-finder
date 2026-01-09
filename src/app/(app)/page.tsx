201406d7ef4c4615e4850cde372ae4c2305c6765
george many <jigumany@gmail.com>
2026-01-09 08:38:59 +0000

    clean up the project i keep getting this error
    Error: ENOENT: no such f

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,1 +1,1 @@
-<!-- This file is intentionally deleted to resolve a Vercel build error. -->
+<!-- This file is intentionally deleted to resolve a Vercel build error. It is being renamed to page.tsx.BAK -->
